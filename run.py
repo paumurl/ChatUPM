833c8f00a52bbdf5063fff456457338738f6592e
paumurl <paularobleslopez@gmail.com>
2023-06-23 16:59:58 +0200

    Add logging to the app launch

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 from waitress import serve
 from app.app import app
+import logging
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000)
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    ip = "127.0.0.1"
+    port = 5000
+    logger.info(f'Starting server on http://{ip}:{port}')
+
+    serve(app, host=ip, port=port)
